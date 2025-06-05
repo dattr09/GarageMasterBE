dbdb6c903f931638f2a326ce3effcf32521df326
NguyenTu11 <tunguyenvan1110@gmail.com>
2025-06-05 12:23:21 +0700

    ...

--- obj/Debug/net9.0/GarageMasterBE.AssemblyInfo.cs
+++ obj/Debug/net9.0/GarageMasterBE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GarageMasterBE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc48cece9883a37c3268b0be39390060307b8c77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af2de47d13fa6cd4346d11bff4caecc2749d3f38")]
 [assembly: System.Reflection.AssemblyProductAttribute("GarageMasterBE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GarageMasterBE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
